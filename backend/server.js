c7b3937bff900d474f9256887d86159b124ba924
mustafateker <tekermustafa038@gmail.com>
2024-12-23 23:27:53 +0300

    MongoDb Bağlantısı yapıldı.

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./db');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
